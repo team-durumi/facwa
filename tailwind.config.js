97daf140c7769e506c901aa0f0653e245f2688cb
wj-mini <woonjjang@gmail.com>
2026-06-02 02:14:46 +0900

    edit layout and sync to archiveJ, renew search and browse

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,20 @@ module.exports = {
   content: [
     '../layouts/**/*.html',
     '../assets/**/*.js',
-    './content/**/*.md',
-    './hugo_stats.json',
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: '40rem',
+        md: '48rem',
+        lg: '64rem',
+        xl: '80rem',
+        '2xl': '102.5rem',
+      },
+    },
     fontFamily: {
       sans: [ '"Noto Sans KR"', ...fontFamily.sans],
     },
   }
 };
-
